dda57f6e95ff036bc137ed25c3e15be96787e372
mast8 <ronald.miranda.rodriguez@gmail.com>
2025-05-25 19:02:19 -0400

    changes color javascript

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ const sidebar = document.querySelector(".sidebar");
 const activeListItem = document.querySelector(".list-item.active");
 
 dark.addEventListener("click", () => {
-  sidebar.className = "sidebar";
-  activeListItem.className = "list-item active";
-  dark.style.display = 'none';
+    sidebar.className = "sidebar";
+    activeListItem.className = "list-item active";
+
+    const activeColor = document.querySelector(".color-box.dark");
+    console.log(activeColor)
+    activeColor.className = "color-box dark active";
+    console.log(activeColor.className)
+    //dark.style.display = 'none';
 });
+
 
 night.addEventListener("click", () => {
     sidebar.className = "sidebar night";
     activeListItem.className = "list-item night active";
     
-    night.style.display = 'none';
-    dark.style.display = 'block';
+    //night.style.display = 'none';
+    
 });
 
 light.addEventListener("click", () => {
     sidebar.className = "sidebar light";
     activeListItem.className = "list-item light active";
+
+
 });
