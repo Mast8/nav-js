61e4d1fe41add84fe4bdf29f6a352cbafaeacb7d
mast8 <ronald.miranda.rodriguez@gmail.com>
2025-05-24 21:18:04 -0400

    hide show buttoms

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const activeListItem = document.querySelector(".list-item.active");
 dark.addEventListener("click", () => {
   sidebar.className = "sidebar";
   activeListItem.className = "list-item active";
+  dark.style.display = 'none';
 });
 
 night.addEventListener("click", () => {
     sidebar.className = "sidebar night";
     activeListItem.className = "list-item night active";
+    
+    night.style.display = 'none';
+    dark.style.display = 'block';
 });
 
 light.addEventListener("click", () => {
